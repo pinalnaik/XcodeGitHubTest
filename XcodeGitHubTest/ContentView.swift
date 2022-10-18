0474cb0ced3d30d9db928cc730389faa2322e3fa
Pinal Naik <pinal.naik@gmail.com>
2022-10-18 14:48:20 +0530

    Initial Commit

---
+++ XcodeGitHubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeGitHubTest
+//
+//  Created by Pinal Naik on 18/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeGitHubTest/XcodeGitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitHubTestApp.swift
+//  XcodeGitHubTest
+//
+//  Created by Pinal Naik on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
